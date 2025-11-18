00c2f1052b62d6205a0e5f09f7fe0e63981fc83d
ezhilgreensomr <ezhilgreensomr@gmail.com>
2025-11-18 20:21:31 +0530

    commit 1

--- src/com/sample/Student.java
+++ src/com/sample/Student.java
@@ -1,10 +1,16 @@
 package com.sample;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Student {
+	
+public static void main(String[] args){
 
-	public static void main(String[] args) {
-		
-		System.out.println("java");
-	}
+System.out.println("java");
+System.out.println("python");
+System.out.println("c++");
+
+
 }
-
+}
